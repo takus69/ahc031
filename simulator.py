f18ba937d952b29371ccd935a5b7ea9618b2ae8f
Takuya Saito <takus4649@gmail.com>
2024-04-02 05:06:04 +0900

    v8: ゼロコストソルバー実装(提出: 57,653,856)

--- simulator.py
+++ simulator.py
@@ -8,6 +8,7 @@ import json
 
 def run(i):
     output_str = subprocess.run(f'powershell cat in/{i:04}.txt | .\\target\\debug\\ahc031.exe > out/{i:04}.txt', shell=True, capture_output=True, text=True).stderr
+    print(output_str)
     result = json.loads(output_str.split('\n')[0])
     return result
 
@@ -31,7 +32,6 @@ if __name__ == '__main__':
     start = time.time()
     trial = 200
     result = []
-    '''
     for i in tqdm(range(trial)):
         r = run(i)
         t = round(time.time()-start, 4)
@@ -39,7 +39,8 @@ if __name__ == '__main__':
         n = r['n']
         e = r['e']
         cost = r['cost']
-        data = [i, d, n, round(e, 4), cost, t]
+        cost2 = r['cost'] / ((d-1)*(n-2))
+        data = [i, d, n, round(e, 4), cost, cost2, t]
         result.append(data)
     '''
     processes = multiprocessing.cpu_count()
@@ -47,6 +48,7 @@ if __name__ == '__main__':
         data = [pool.apply_async(main, (i,)) for i in range(trial)]
         result = [d.get() for d in data]
     print()
+    '''
     df = pd.DataFrame(result, columns=['i', 'd', 'n', 'e', 'cost', 'cost2', 'time'])
     cost = np.mean(df['cost']) * 50
     cost2 = np.mean(df['cost2']) * 50
