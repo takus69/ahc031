8bb9d256056c88f8e01ebb3586f90cdcd2ab671d
Takuya Saito <takus4649@gmail.com>
2024-04-17 00:27:57 +0900

    縦のパーティションを固定。ランダムにパーティションの数と位置を決めて、詰め込んでいく

---
+++ simulator_sample.py
@@ -0,0 +1,57 @@
+import numpy as np
+from tqdm import tqdm
+import pandas as pd
+import time
+import multiprocessing
+import subprocess
+import json
+
+def run(i):
+    output_str = subprocess.run(f'powershell cat in/{i:04}.txt | .\\target\\debug\\sample06.exe > out/{i:04}.txt', shell=True, capture_output=True, text=True).stderr
+    # print(output_str)
+    result = json.loads(output_str.split('\n')[0])
+    return result
+
+def main(i):
+    start = time.time()
+    # print(i, 'start')
+    r = run(i)
+    t = round(time.time()-start, 4)
+    d = r['d']
+    n = r['n']
+    e = r['e']
+    cost = r['cost']
+    cost2 = r['cost'] / ((d-1)*(n-2))
+    data = [i, d, n, round(e, 4), cost, cost2, t]
+    print('\r', 'end', i, end='')
+    # print(i, 'end')
+    return data
+
+
+if __name__ == '__main__':
+    start = time.time()
+    trial = 200
+    result = []
+    '''
+    for i in tqdm(range(trial)):
+        r = run(i)
+        t = round(time.time()-start, 4)
+        d = r['d']
+        n = r['n']
+        e = r['e']
+        cost = r['cost']
+        cost2 = r['cost'] / ((d-1)*(n-2))
+        data = [i, d, n, round(e, 4), cost, cost2, t]
+        result.append(data)
+    '''
+    processes = multiprocessing.cpu_count()
+    with multiprocessing.Pool(processes=processes) as pool:
+        data = [pool.apply_async(main, (i,)) for i in range(trial)]
+        result = [d.get() for d in data]
+    print()
+    df = pd.DataFrame(result, columns=['i', 'd', 'n', 'e', 'cost', 'cost2', 'time'])
+    cost = np.mean(df['cost']) * 50
+    cost2 = np.mean(df['cost2']) * 50
+    print(f'cost:', format(int(df['cost'].sum()*50/trial), ','), 'cost2:', format(int(df['cost2'].mean()), ','))
+    df.to_csv('result.csv', index=False)
+    print(f'end elapsed time: {time.time()-start:.2f}s')
--- src/bin/sample06.rs
+++ src/bin/sample06.rs
@@ -1,4 +1,7 @@
 use proconio::input;
+use std::collections::HashSet;
+use rand::prelude::*;
+use std::time::{Duration, Instant};
 
 struct Input {
     w: usize,
@@ -15,15 +18,13 @@ fn parse_input() -> Input {
     Input { w, d, n, a }
 }
 
+#[derive(Clone)]
 struct Output {
     rect: Vec<Vec<(usize, usize, usize, usize)>>,
 }
 
 fn compute_score(input: &Input, out: &Output) -> (i64, String) {
-    let (mut score, err) = compute_score_details(input, &out.rect);
-    if !err.is_empty() {
-        score = 0;
-    }
+    let (score, err) = compute_score_details(input, &out.rect);
     (score, err)
 }
 
@@ -32,15 +33,19 @@ fn compute_score_details(
     out: &[Vec<(usize, usize, usize, usize)>],
 ) -> (i64, String) {
     let mut score = 0;
+    let max_score: i64 = 1_000_000_000_000;
     let mut hs: Vec<(usize, usize, usize)> = Vec::new();
-    let mut vs: Vec<(usize, usize, usize)> = Vec::new();
+    // let mut vs: Vec<(usize, usize, usize)> = Vec::new();
     for (d, _) in out.iter().enumerate() {
         for p in 0..input.n {
+            if out[d][p].2 > input.w || out[d][p].3 > input.w {
+                return (max_score, format!("Over {}'s w on day {}.", p, d));
+            }
             for q in 0..p {
                 if out[d][p].2.min(out[d][q].2) > out[d][p].0.max(out[d][q].0)
                     && out[d][p].3.min(out[d][q].3) > out[d][p].1.max(out[d][q].1)
                 {
-                    return (0, format!("Rectangles {} and {} overlap on day {}.", q, p, d));
+                    return (max_score, format!("Rectangles {} and {} overlap on day {}.", q, p, d));
                 }
             }
         }
@@ -62,45 +67,33 @@ fn compute_score_details(
 
         if d == 0 {
             hs = hs2;
-            vs = vs2;
+            // vs = vs2;
             continue;
         }
         // 横の撤去
         for a in &hs {
             if a.0 == 0 || a.0 == input.w { continue; }
             score += a.2 as i64 - a.1 as i64;
-            for b in &hs2 {
-                let r = (a.2 as i64).min(b.2 as i64);
-                let l = (a.1 as i64).max(b.1 as i64);
-                if a.0 == b.0 && l < r{
-                    score -= r - l;
-                }
-            }
         }
         // 縦の撤去
+        /*
         for a in &vs {
             if a.0 == 0 || a.0 == input.w { continue; }
             score += a.2 as i64 - a.1 as i64;
-            for b in &vs2 {
-                let r = (a.2 as i64).min(b.2 as i64);
-                let l = (a.1 as i64).max(b.1 as i64);
-                if a.0 == b.0 && l > r{
-                    score -= l - r;
-                }
-            }
-        }
+        }*/
         // 横の設置
         for a in &hs2 {
             if a.0 == 0 || a.0 == input.w { continue; }
             score += a.2 as i64 - a.1 as i64;
         }
         // 縦の設置
+        /*
         for a in &vs2 {
             if a.0 == 0 || a.0 == input.w { continue; }
             score += a.2 as i64 - a.1 as i64;
-        }
+        }*/
         hs = hs2;
-        vs = vs2;
+        // vs = vs2;
     }
     (score + 1, String::new())
 }
@@ -113,22 +106,103 @@ fn ans(output: &Output) {
     }
 }
 
+fn decide_partition(a: &[usize], n: usize, w: usize, k: usize, vs: &[usize]) -> Vec<(usize, usize, usize, usize)> {
+    let mut rect: Vec<(usize, usize, usize, usize)> = Vec::with_capacity(n);
+    let mut hs = vec![0; k];
+    let mut free = Vec::with_capacity(k);
+    let mut v1 = &vs[0];
+    for v2 in &vs[1..] {
+        free.push(w*(v2-v1));
+        v1 = v2;
+    }
+
+    for ai in a.iter().rev() {
+        let mut target_i = 0;
+        let mut min_free = w * w;
+        let mut v1 = vs[0];
+        for i in 0..k {
+            let v2 = vs[i+1];
+            let h = ai / (v2 - v1) + 1;
+            if h + hs[i] > w { continue; }
+            let bi = (v2 - v1) * h;
+            let f = free[i];
+            // eprintln!("min free: {}, f: {}, bi: {}", min_free, f, bi);
+            if f >= bi {
+                if min_free > f - bi {
+                    min_free = f - bi;
+                    target_i = i;
+                }
+            } else { continue; }
+            v1 = v2;
+        }
+
+        let v1 = vs[target_i];
+        let v2 = vs[target_i+1];
+        let h = ai / (v2 - v1) + 1;
+        free[target_i] = min_free;
+        let h1 = hs[target_i];
+        let mut h2 = h1 + h;
+        if h2 > w { h2 = h1 + 1; }
+        hs[target_i] = h2;
+        rect.push((h1, v1, h2, v2));
+    }
+    rect.reverse();
+    rect
+}
+
+fn rnd_split(input: &Input, rng: &mut dyn RngCore) -> (usize, Vec<usize>) {
+    let k = rng.gen_range(2i32..input.n as i32) as usize;
+    let mut vs = HashSet::new();
+    for _ in 0..k {
+        let v = rng.gen_range(1i32..=input.w as i32) as usize;
+        vs.insert(v);
+    }
+    vs.insert(0);
+    vs.insert(input.w);
+    let mut vs: Vec<usize> = vs.into_iter().collect();
+    vs.sort();
+    let k = vs.len()-1;
+    // eprintln!("rnd k: {}, vs: {:?}", k, vs);
+
+    (k, vs)
+}
+
+fn result(input: &Input, cost: i64) {
+    let sums: Vec<usize> = input.a.iter().map(|row| row.iter().sum()).collect();
+    let average: f64 = sums.iter().sum::<usize>() as f64 / sums.len() as f64;
+    let e = (1.0 - average / (input.w * input.w) as f64).sqrt();
+    eprintln!("{{ \"d\": {}, \"n\": {}, \"e\": {}, \"cost\": {} }}", input.d, input.n, e, cost);
+}
+
 fn main() {
+    let start = Instant::now();
+
     let input: Input = parse_input();
     let rect: Vec<Vec<(usize, usize, usize, usize)>> = vec![Vec::with_capacity(input.n); input.d];
     let mut output = Output{ rect };
 
-    for d in 0..input.d {
-        let rd = &mut output.rect[d];
-        let mut k1 = 0;
-        let mut k2 = 0;
-        for k in 0..input.n {
-            k2 += (input.a[d][k] / input.w) + 1;
-            rd.push((k1, 0, k2.min(input.w), input.w));
-            k1 = k2;
+    let seed = 0;
+    let mut rng = rand_chacha::ChaCha20Rng::seed_from_u64(seed);
+
+    let mut best_output = output.clone();
+    let mut best_cost = i64::MAX;
+    for _ in 0..100000 {
+        if start.elapsed() >= Duration::from_secs_f64(2.8) {
+            // eprintln!("Time up!!!");
+            break;
+        }
+        let (k, vs) = rnd_split(&input, &mut rng);
+        for d in 0..input.d {
+            output.rect[d] = decide_partition(&input.a[d], input.n, input.w, k, &vs);
+        }
+        let (cost, _) = compute_score(&input, &output);
+        if best_cost > cost {
+            best_cost = cost;
+            best_output = output.clone();
         }
     }
-    ans(&output);
-    let (cost, _) = compute_score(&input, &output);
-    eprintln!("cost: {}", cost);
+    ans(&best_output);
+    let (cost, _) = compute_score(&input, &best_output);
+    result(&input, cost);
+    // eprintln!("cost: {}, s: {}", cost, s);
 }
